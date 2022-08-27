ac958b88c81160dfacb9a72b58871d67a5f28198
JohnD <john.donoghue@ieee.org>
2022-08-27 11:46:16 -0400

    * inst/test_netcdf.m: add texinfo docs to test_netcdf

--- inst/test_netcdf.m
+++ inst/test_netcdf.m
@@ -1,11 +1,27 @@
-% test_netcdf
-% Test the netcdf interface
+%% Copyright (C) 2013-2022 Alexander Barth
+%%
+%% This program is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation; either version 2 of the License, or
+%% (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with this program; If not, see <http://www.gnu.org/licenses/>.
 
+## -*- texinfo -*-
+## @deftypefn {} {} test_netcdf
+## Function to do a basic test of the netcdf interface
+## @end deftypefn
 function test_netcdf()
 
-import_netcdf
+  import_netcdf
 
-tests = {'test_netcdf_constant',...
+  tests = {'test_netcdf_constant',...
          'test_netcdf_create'...
          'test_netcdf_low_level_interface'...
          'test_netcdf_unlim',...         
@@ -20,38 +36,24 @@ tests = {'test_netcdf_constant',...
          'bug_47014'...
         };
 
-maxlen = max(cellfun(@(s) length(s),tests));
+  maxlen = max(cellfun(@(s) length(s),tests));
 
-libver = netcdf.inqLibVers();
-fprintf('Using NetCDF library version "%s"\n',libver)
+  libver = netcdf.inqLibVers();
+  fprintf('Using NetCDF library version "%s"\n',libver)
 
-for iindex=1:length(tests);
+  for iindex=1:length(tests);
 
-  dots = repmat('.',1,maxlen - length(tests{iindex}));
-  fprintf('run %s%s ',tests{iindex},dots);
-  try
-    eval(tests{iindex});
-    disp('  OK  ');
-  catch
-    disp(' FAIL ');
-    disp(lasterr)
-  end
-end
-
-%% Copyright (C) 2013-2019 Alexander Barth
-%%
-%% This program is free software; you can redistribute it and/or modify
-%% it under the terms of the GNU General Public License as published by
-%% the Free Software Foundation; either version 2 of the License, or
-%% (at your option) any later version.
-%%
-%% This program is distributed in the hope that it will be useful,
-%% but WITHOUT ANY WARRANTY; without even the implied warranty of
-%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%% GNU General Public License for more details.
-%%
-%% You should have received a copy of the GNU General Public License
-%% along with this program; If not, see <http://www.gnu.org/licenses/>.
+    dots = repmat('.',1,maxlen - length(tests{iindex}));
+    fprintf('run %s%s ',tests{iindex},dots);
+    try
+      eval(tests{iindex});
+      disp('  OK  ');
+    catch
+     disp(' FAIL ');
+      disp(lasterr)
+    end_try_catch
+  endfor
+endfunction
 
 %!test
 %! fprintf ("\n");
